b53e839a0530d70b3583bdb7729a27578f102ec5
李常国 <1282128587@qq.com>
2024-03-04 14:38:56 +0800

    useGetState

---
+++ packages/hooks/src/useGetState/index.ts
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useCallback } from 'react';
+
+function useGetState<S>(initialState?: S) {
+  const [state, setState] = useState(initialState);
+  const stateRef = useRef(state);
+  stateRef.current = state;
+
+  const getState = useCallback(() => stateRef.current, []);
+
+  return [state, setState, getState];
+}
+
+export default useGetState;
