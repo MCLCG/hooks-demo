ba68ed0c31127659a0ad0f94da3dcf1a1e772c60
李常国 <1282128587@qq.com>
2024-03-04 15:02:36 +0800

    useContext

--- packages/hooks/src/useGetState/index.ts
+++ packages/hooks/src/useGetState/index.ts
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback, useMemo } from 'react';
+import React, { useState, useRef, useCallback, useMemo, useContext } from 'react';
 
 function useGetState<S>(initialState?: S) {
   const [state, setState] = useState(initialState);
